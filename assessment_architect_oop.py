dd576cf7a4bcc07d5a1ccc1bdf63bdb53538a5cc
Andre <andrepetilunalolong@gmail.com>
2025-05-28 01:32:14 +0800

    removed the duplicate of the code from the beginning

--- assessment_architect_oop.py
+++ assessment_architect_oop.py
@@ -1,33 +1,3 @@
-def quiz():
-    with open("Questionnaire.txt", "a") as file:
-         while True:
-            # ask the question
-            question = input("Enter your desired question: ").capitalize()
-            # ask four possible answers (a, b, c, d)
-            choice_a = input("Enter choice letter a: ")
-            choice_b = input("Enter choice letter b: ")
-            choice_c = input("Enter choice letter c: ")
-            choice_d = input("Enter choice letter d: ")
-            # ask the correct answer
-            correct = input("Which letter is correct among the choices a, b, c, and d?: ")
-            # write the inputs to file
-            file.write("Question: " + question + "\n")
-            file.write("a.): " + choice_a + "\n")
-            file.write("b.): " + choice_b + "\n")
-            file.write("c.): " + choice_c + "\n")
-            file.write("d.): " + choice_d + "\n")
-            file.write("correct answer: " + correct + "\n")
-            print(" ")
-
-            continunity = input("Do you want to add another question? (y/n): ")
-            if continunity.lower() != "y":
-                break       
-        
-    print("Your question(s) and choice(s) as well as the right answer has been added to the file")
-    print("Your questionnaire is now ready!")
-        
-quiz()
-
 # Create a class 
 # Create a constructor for the 6 inputs
 #           question
